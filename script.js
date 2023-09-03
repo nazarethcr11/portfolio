02e57476a2bdb832c3d469b173f1390b1facd8f9
Silene Nazareth Casma Rubio <u20201a213@upc.edu.pe>
2023-09-02 21:35:53 -0500

    add: all files

---
+++ script.js
@@ -0,0 +1,15 @@
+const sr= ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.services-content',{delay:100});
+sr.reveal('.box',{delay:200});
+
+sr.reveal('.app',{delay:100});
+sr.reveal('.app-content',{delay:200});
+
+sr.reveal('.contact',{delay:100});
+sr.reveal('.contact-form',{delay:200});
